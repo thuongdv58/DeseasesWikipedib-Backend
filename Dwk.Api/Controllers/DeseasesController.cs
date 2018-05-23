51a7c6a19e8d0e6a7273fc5c6b0ecaa041e754eb
thuongdv <thuongdv95@gmail.com>
2018-05-23 07:24:03 +0700

    project initial

---
+++ Dwk.Api/Controllers/DeseasesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dwk.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Deseases")]
+    public class DeseasesController : Controller
+    {
+        // GET: api/Deseases
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "HIV", "Diabetes" };
+        }
+
+        // GET: api/Deseases/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "AIDS";
+        }
+        
+        // POST: api/Deseases
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Deseases/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
